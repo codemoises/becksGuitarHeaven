3a1efa403e542b8bb45da3d69a3a5ec766bcd2e9
Moises Cruz Braga <moisesbraga223@gmail.com>
2024-03-08 13:51:01 -0300

    navlinks

--- src/components/NavLinks/index.tsx
+++ src/components/NavLinks/index.tsx
@@ -1,34 +1,65 @@
 import NextLink from 'next/link'
-import { Flex, Box, Link } from "@chakra-ui/react";
+import { Flex, Box, Link, Heading } from "@chakra-ui/react";
 
 export default function NavLinks() {
     return (
-        <Flex padding={"32px 0"} alignItems={"center"} justifyContent={"center"}>
-            <Box className='linkHovered'>
-                <Link as={NextLink} href={"#"}>
-                    Modelos
-                </Link>
+        <Box>
+            <Flex padding={"32px 0 20px 0"} alignItems={"center"} justifyContent={"center"}>
+                <Box className='linkHovered'>
+                    <Link as={NextLink} href={"#"}>
+                        Modelos
+                    </Link>
+                </Box>
+                <Box className='linkHovered'>
+                    <Link as={NextLink} href={"#"}>
+                        Elétricas
+                    </Link>
+                </Box>
+                <Box className='linkHovered'>
+                    <Link as={NextLink} href={"#"}>
+                        Acústicas
+                    </Link>
+                </Box>
+                <Box className='linkHovered'>
+                    <Link as={NextLink} href={"#"}>
+                        Sobre nós
+                    </Link>
+                </Box>
+                <Box className='linkHovered'>
+                    <Link as={NextLink} href={"#"}>
+                        Contato
+                    </Link>
+                </Box>
+            </Flex>
+            <Box className={""} boxShadow={"0 20px 30px 0 rgba(0, 0, 0, .3)"}>
+                <Box maxWidth={"1420px"} margin={"0 auto"} padding={"16px 62px"}>
+                    <Box marginBottom={"22px"} borderBottom={"1px solid #bfc0c1"} padding={"18px 0"}>
+                        <Heading fontSize={"18px"}>Elétricas</Heading>
+                    </Box>
+                    <Flex flexDirection={"column"}>
+                        <Box paddingBottom={"12px"}>
+                            <Link as={NextLink} href={"#"}>
+                                Stratocaster
+                            </Link>
+                        </Box>
+                        <Box paddingBottom={"12px"}>
+                            <Link as={NextLink} href={"#"}>
+                                Stratocaster
+                            </Link>
+                        </Box>
+                        <Box paddingBottom={"12px"}>
+                            <Link as={NextLink} href={"#"}>
+                                Stratocaster
+                            </Link>
+                        </Box>
+                        <Box paddingBottom={"12px"}>
+                            <Link as={NextLink} href={"#"}>
+                                Stratocaster
+                            </Link>
+                        </Box>
+                    </Flex>
+                </Box>
             </Box>
-            <Box className='linkHovered'>
-                <Link as={NextLink} href={"#"}>
-                    Modelos
-                </Link>
-            </Box>
-            <Box className='linkHovered'>
-                <Link as={NextLink} href={"#"}>
-                    Acústicas
-                </Link>
-            </Box>
-            <Box className='linkHovered'>
-                <Link as={NextLink} href={"#"}>
-                    Sobre nós
-                </Link>
-            </Box>
-            <Box className='linkHovered'>
-                <Link as={NextLink} href={"#"}>
-                    Contato
-                </Link>
-            </Box>
-        </Flex>
+        </Box>
     );
 };
